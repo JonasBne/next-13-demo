bf85d35fab016759969ac94df85d4345b361fd4e
Jonas Boone <boone.jonas@outlook.be>
2023-06-07 09:01:28 +0200

    feat: add repository page with server component and fetch

--- src/app/code/repositories/page.tsx
+++ src/app/code/repositories/page.tsx
@@ -1,3 +1,43 @@
-export default function RepositoriesPage() {
-  return <h1>Repositories</h1>;
+import Link from 'next/link';
+import { FaCodeBranch, FaEye, FaStar } from 'react-icons/fa';
+
+async function fetchRepositories() {
+  const response = await fetch('https://api.github.com/users/jonasbne/repos');
+  const repositories = await response.json();
+  return repositories;
+}
+
+export default async function RepositoriesPage() {
+  const repositories = await fetchRepositories();
+  return (
+    <div className='flex flex-col items-center'>
+      <h2 className='mb-4'>Repositories</h2>
+      <ul>
+        {repositories.map((repository: any) => (
+          <li className='bg-gray-700 mb-4 rounded-md p-4' key={repository.id}>
+            <Link href={`/code/repositories/${repository.name}`}>
+              <h3 className='font-bold'>{repository.name}</h3>
+              <p className='font-light text-sm'>
+                Description: {repository.description || 'n/a'}
+              </p>
+              <div className='flex justify-between mt-2'>
+                <div className='flex items-center gap-x-2'>
+                  <FaStar />
+                  {repository.stargazers_count}
+                </div>
+                <div className='flex items-center gap-x-2'>
+                  <FaCodeBranch />
+                  {repository.forks_count}
+                </div>
+                <div className='flex items-center gap-x-2'>
+                  <FaEye />
+                  {repository.watchers_count}
+                </div>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export const Header = () => {
   return (
     <header className='border border-slate-100 flex flex-col items-center p-4 mb-4'>
-      <h1 className='text-2xl font-bold tracking-wider'>Github finder</h1>
+      <h1 className='text-2xl mb-4 font-bold tracking-wider'>
+        Github profile analyzer
+      </h1>
       <ul className='flex gap-x-4'>
         <Link href='/'>Home</Link>
         <Link href='/about'>About</Link>
